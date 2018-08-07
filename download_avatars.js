9b2f168bb08b661a08ad9f0693c41bc29b6ce43e
Julia <juliazhouism@gmail.comgit config --global user.email juliazhouism@gmail.comgit confit --global user.name Julia>
2018-08-07 04:26:47 +0000

    images downloaded and support line arguements supported and required

--- download_avatars.js
+++ download_avatars.js
@@ -9,53 +9,47 @@ var repoOwner = process.argv[2];
 var repoName = process.argv[3];
 
 
-var cb = function callback(obj){
-  for (var i = 0; i < obj.length; i++) {
-    var filePath = obj.login;
-    var url = obj.avatar_url;
-  }
-};
-
-
 function getRepoContributors(repoOwner, repoName, cb) {
+
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
-      'Authentication' : token.GITHUB_TOKEN,
-      'User-Agent': 'request'
-    }
-  };
+      'User-Agent': 'request',
+      'Authentication': token.GITHUB_TOKEN
+  }
+};
 
-  request(options, function(err, res, body) {
-    cb(err, body);
-    var obj = JSON.parse(body);
-    console.log(obj); //=> returns as objects
-  });
+    request(options, function(err, res, body) {
+      cb(err, JSON.parse(body));
+    });
 }
 
 function downloadImageByURL(url, filePath) {
-  request.get(url)
-       .on('error', function (err) {
-         console.log("error");
-         throw err;
-       })
-       .on('response', function (response) {
-        console.log('Downloading Image')
-         console.log('Response Status Code: ', response.statusCode);
-         console.log('Download Complete.');
-       })
-       .pipe(fs.createWriteStream(filePath));
-}
+    request.get(url)
+         .on('error', function (err) {
+           console.log("error");
+           throw err;
+         })
+         .on('response', function (response) {
+          console.log('Downloading Image')
+          console.log('Response Status Code: ', response.statusCode);
+          console.log('Download Complete.');
+         })
+         .pipe(fs.createWriteStream(filePath + '.jpg'));
+  };
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
-//
 
-getRepoContributors("jquery", "jquery", function(err, result) {
-  console.log("Errors:", err);
-  console.log("Result:", result);
+getRepoContributors("jquery", "jquery", function(err, results) {
+  //console.log("Errors:", err);
+  console.log('results:', results);
+  for(var i  = 0; i < results.length; i++){
+    //console.log(results[i]); => prints all the avatar_url
+    if (!repoOwner || !repoName) {
+      console.log('Please provide the repo owner and name.');
+    } else {
+    downloadImageByURL(results[i].avatar_url, './avatars/' + results[i].login);
+  }
+};
 });
-
-
-
-
