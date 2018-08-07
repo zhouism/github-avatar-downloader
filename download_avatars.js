742b2b6e6b7841e7a8340056146171a63e734f94
Julia <juliazhouism@gmail.comgit config --global user.email juliazhouism@gmail.comgit confit --global user.name Julia>
2018-08-07 02:29:01 +0000

    prints avatar URL

--- download_avatars.js
+++ download_avatars.js
@@ -1,22 +1,50 @@
 var request = require('request');
 
+var token = require('./secrets');
+
+var fs = require('fs');
+
 var repoOwner = process.argv[2];
 
 var repoName = process.argv[3];
 
-var token = require('secrets');
+
+var cb = function callback(obj){
+  for (var i = 0; i < obj.length; i++) {
+    var filePath = obj.login;
+    var url = obj.avatar_url;
+  }
+};
+
 
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
+      'Authentication' : token.GITHUB_TOKEN,
       'User-Agent': 'request'
     }
   };
 
   request(options, function(err, res, body) {
     cb(err, body);
+    var obj = JSON.parse(body);
+    console.log(obj); //=> returns as objects
   });
+}
+
+function downloadImageByURL(url, filePath) {
+  request.get(url)
+       .on('error', function (err) {
+         console.log("error");
+         throw err;
+       })
+       .on('response', function (response) {
+        console.log('Downloading Image')
+         console.log('Response Status Code: ', response.statusCode);
+         console.log('Download Complete.');
+       })
+       .pipe(fs.createWriteStream(filePath));
 }
 
 console.log('Welcome to the GitHub Avatar Downloader!');
@@ -29,6 +57,5 @@ getRepoContributors("jquery", "jquery", function(err, result) {
 });
 
 
-var cb = function callback(){
 
-};
+
